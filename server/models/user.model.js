37c22b66c2a6125d81945f0f20c6f4da642a5902
AJ <arunjagadishsd@gmail.com>
2020-09-01 11:35:00 +0530

    Add user model

---
+++ server/models/user.model.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
+
+module.exports = model("User", userSchema);
